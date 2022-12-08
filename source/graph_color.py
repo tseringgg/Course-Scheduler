3ff56845599d5676dc0a9577f54bdd2b2676ad9d
cweldy23 <connorweldy@outlook.com>
2022-12-07 16:11:28 -0800

    fixed case with only 1 timeslot

--- source/graph_color.py
+++ source/graph_color.py
@@ -8,18 +8,21 @@ from course import *
 #         number of rooms available in total as an integer, and a list of neighbors to the course
 #outputs - no return values but sets the room value of the current course (colors it)
 def set_timeslot(current_course, num_avail_timeslots, neighbor_timeslot):
-    max_room = 0
-    #courses_per_timeslot
+    max_timeslot = 0
     #while current_course.get_room() == -1: #while the course's room not set
     for timeslot in range(1,num_avail_timeslots+1):
 
-        # check how many times r has been used
+        # check how many times timeslot has been used
+        #number_of_times(timeslot) < rooms_available:
         
         if timeslot not in neighbor_timeslot:
             current_course.set_timeslot(timeslot)
-            max_room = timeslot
-            break
-    return max_room
+            max_timeslot = timeslot
+            break # gets lowest available timeslot
+    
+    
+        
+    return max_timeslot
     
 
 # create_graph_coloring_greedy function - colors a graph using a greedy algorithm. uses https://iq.opengenus.org/graph-colouring-greedy-algorithm/ as a reference
@@ -31,6 +34,7 @@ def set_timeslot(current_course, num_avail_timeslots, neighbor_timeslot):
 # output: returns a set of Courses, with all items having the color assigned using graph
 #           coloring methods.
 def create_graph_coloring_greedy(adj_list, num_avail_rooms):
+    
     # Color first vertex with first color
     # Repeat following for V-1 times:
     # - consider the currently picked vertex
@@ -55,14 +59,18 @@ def create_graph_coloring_greedy(adj_list, num_avail_rooms):
     #graph coloring with BFS
     #adj_list[0].set_room(1)
     bfs_queue = []
+    visited = set()
     for course in adj_list: #make sure we cover all the courses
         if course.get_room() == -1: # if course does not have a room yet
             bfs_queue.append(course) # add to end of queue
         while bfs_queue: #while the queue isnt empty
+            print(f"queue: {bfs_queue}")
             current_course = bfs_queue.pop(0) # get first course from queue
+            visited.add(current_course)
             if current_course.get_room() == -1: # if course does not have a room yet
                 # Get rooms of neighbors:
                 neighbor_timeslot = current_course.get_neighbor_timeslots()
+
                 print(current_course.course_id)
                 print(neighbor_timeslot)
                 print(current_course.get_neighbors())
@@ -71,6 +79,6 @@ def create_graph_coloring_greedy(adj_list, num_avail_rooms):
                 # Add neighbor nodes that don't have a room
                 for neighbor_course in current_course.get_neighbors():
                     #add the neighbor_course to the queue if the course isn't in the queue
-                    #and if the course has not been assigned a room yet.
-                    if neighbor_course not in bfs_queue and neighbor_course.get_timeslot() == -1:
+                    #and if the course has not been visited yet.
+                    if (neighbor_course not in bfs_queue) and (neighbor_course not in visited):
                         bfs_queue.append(neighbor_course)
